0367874e04a22f7e4027cf5e1cd22fa804d1efa2
Harshaja Agnihotri <hn.agnihotri29@gmail.com>
2024-09-11 20:37:45 +0530

    [Contact] Added the new contact component with style.

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import AboutMe from "./components/AboutMe/AboutMe";
+import Contact from "./components/Contact/Contact";
 import Experience from "./components/Experiences/Experience";
 import Home from "./components/Home/Home";
 import Navbar from "./components/Navbar/Navbar";
@@ -12,6 +13,7 @@ function App() {
       <AboutMe />
       <Experience />
       <Project />
+      <Contact />
     </div>
   );
 }
---
+++ src/components/Contact/Contact.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import './Contact.style.scss';
+
+const Contact = () => {
+    return (
+        <div className='contact'>
+            <div className='container'>
+                <div className='contact_content'>
+                    <div className='contact_title'>
+                        <p>contact</p>
+                        <h3>Don't be shy! Hit me up! 👇</h3>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Contact
