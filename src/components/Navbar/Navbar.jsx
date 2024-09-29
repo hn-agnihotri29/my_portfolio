5761144206e61bf3480cb1a20080b1f5ed2e8b01
hn-agnihotri29 <hn.agnihotri29@gmail.com>
2024-09-29 20:15:33 +0530

    [Navbar] styling for mobile

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,11 +1,22 @@
 import './Navbar.style.scss'
+import { useState } from 'react';
 
 const Navbar = () => {
+
+    const [isOpen, setIsOpen] = useState(false);
+
+    const toggleMenu = () => {
+        setIsOpen(!isOpen);
+    };
+
     return (
         <>
             <nav>
                 <h2 className='logo'>Hna.dev</h2>
-                <ul className='navLink'>
+                <div className='menu-icon' onClick={toggleMenu}>
+                    {isOpen ? '✖' : '☰'}
+                </div>
+                <ul className={`navLink ${isOpen ? 'active' : ''}`}>
                     <li>Home</li>
                     <li>About</li>
                     <li>Experience</li>
